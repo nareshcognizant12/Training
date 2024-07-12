7890409e1962f98cf8aa4356d73ad27f09735ceb
nareshcognizant12 <pappireddy.nareshkumarreddy2@cognizant.com>
2024-07-12 08:32:29 +0530

    Committing local changes

--- FundamentalsPart02/arrowfunction.js
+++ FundamentalsPart02/arrowfunction.js
@@ -4,3 +4,9 @@ console.log(add(5,6));
 const print=()=>console.log("This is an arrow function");
 print();
 
+const yearsUntilRetirement=(birthYear)=>{
+    const age=2037-birthYear;
+    const retirement=65-age;
+    return retirement;
+};
+console.log(yearsUntilRetirement(1999));
